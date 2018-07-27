757b4746b09fabbf222632ed546ac91191d9d523
rafaelucena <rafael.boszko@gmail.com>
2018-07-27 23:29:51 +0200

    "#0003 - Eloquent to Doctrine (Category entity protected to public attributes)"

--- app/Http/Models/_Category.php
+++ app/Http/Models/_Category.php
@@ -9,14 +9,14 @@ use Doctrine\ORM\Mapping AS ORM;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity()
  * @ORM\Table(name="category")
  */
 class _Category
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer", nullable=false)
      */
     public $id;
