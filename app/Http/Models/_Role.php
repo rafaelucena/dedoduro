5d26db08f619541d8b49ba08845b30e1fdf3ea24
rafaelucena <rafael.boszko@gmail.com>
2018-07-27 22:56:53 +0200

    "#0003 - Eloquent to Doctrine (Role entity protected to public attributes)"

--- app/Http/Models/_Role.php
+++ app/Http/Models/_Role.php
@@ -3,8 +3,9 @@
 namespace App\Http\Models;
 
 use App\Http\Models\_UserRole;
-use Doctrine\ORM\Mapping AS ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping AS ORM;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 
 /**
  * @ORM\Entity
@@ -17,30 +18,49 @@ class _Role
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer", nullable=false)
      */
-    protected $id;
+    public $id;
 
     /**
      * @ORM\Column(type="string", length=127, nullable=false)
      */
-    protected $role;
+    public $role;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=false)
      */
-    protected $description;
+    public $description;
 
     /**
      * @ORM\Column(type="datetime", nullable=false)
      */
-    protected $createdAt;
+    public $createdAt;
 
     /**
      * @ORM\Column(type="datetime", nullable=false)
      */
-    protected $updatedAt;
+    public $updatedAt;
 
     /**
      * @ORM\OneToMany(targetEntity="_UserRole", mappedBy="role")
      */
-    protected $usersRole;
+    public $usersRole;
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = time();
+    }
+
+    /**
+     * @param PreUpdateEventArgs $eventArgs
+     * @ORM\PreUpdate()
+     */
+    public function onPreUpdate(PreUpdateEventArgs $eventArgs)
+    {
+        if (!empty($eventArgs->getEntityChangeSet())) {
+            $this->updatedAt = time();
+        }
+    }
 }
