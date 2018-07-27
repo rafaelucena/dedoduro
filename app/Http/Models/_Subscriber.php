660d79c2d56d8d4b1a7ee341366122b1ad041dd5
rafaelucena <rafael.boszko@gmail.com>
2018-07-27 23:29:01 +0200

    "#0003 - Eloquent to Doctrine (Subscriber entity protected to public attributes)"

--- app/Http/Models/_Subscriber.php
+++ app/Http/Models/_Subscriber.php
@@ -40,7 +40,7 @@ class _Subscriber
     public $createdAt;
 
     /**
-     * @ORM\Column(type="datetime", nullable=false)
+     * @ORM\Column(type="datetime", nullable=true)
      */
     public $updatedAt;
 
