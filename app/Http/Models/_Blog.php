de5c9611e73ef61e2c9e293fa4f35d1b14203037
rafaelucena <rafael.boszko@gmail.com>
2018-07-27 22:47:02 +0200

    "#0003 - Eloquent to Doctrine (Blog entity with better annotations as sample)"

--- app/Http/Models/_Blog.php
+++ app/Http/Models/_Blog.php
@@ -23,71 +23,85 @@ class _Blog
     protected $id;
 
     /**
+     * @var string
      * @ORM\Column(type="string", length=127, nullable=false)
      */
     protected $title;
 
     /**
+     * @var string
      * @ORM\Column(type="string", length=127, nullable=false)
      */
     protected $slug;
 
     /**
+     * @var string
      * @ORM\Column(type="string", length=255, nullable=false)
      */
     protected $image;
 
     /**
+     * @var string
      * @ORM\Column(type="text", nullable=false)
      */
     protected $excerpt;
 
     /**
+     * @var string
      * @ORM\Column(type="text", nullable=false)
      */
     protected $description;
 
     /**
+     * @var integer
      * @ORM\Column(type="integer", nullable=false)
      */
     protected $views;
 
     /**
+     * @var integer
      * @ORM\Column(type="integer", nullable=false)
      */
     protected $isActive;
 
     /**
+     * @var integer
      * @ORM\Column(type="integer", nullable=false)
      */
     protected $allowComments;
 
     /**
+     * @var \DateTime
      * @ORM\Column(type="datetime", nullable=false)
      */
     protected $createdAt;
 
     /**
+     * @var \DateTime
      * @ORM\Column(type="datetime", nullable=true)
      */
     protected $updatedAt;
 
     /**
+     * @var \DateTime
      * @ORM\Column(type="datetime", nullable=true)
      */
     protected $deletedAt;
 
     /**
+     * @var _User
      * @ORM\ManyToOne(targetEntity="_User", inversedBy="blogs")
      */
     protected $user;
 
     /**
+     * @var ArrayCollection|_Comment[]
      * @ORM\OneToMany(targetEntity="_Comment", mappedBy="blog")
      */
     protected $comments;
 
     /**
+     * @var ArrayCollection|_BlogCategory[]
      * @ORM\OneToMany(targetEntity="_BlogCategory", mappedBy="blog")
      */
     protected $blogCategories;
@@ -102,7 +116,6 @@ class _Blog
 
     /**
      * @param PreUpdateEventArgs $eventArgs
-     *
      * @ORM\PreUpdate()
      */
     public function onPreUpdate(PreUpdateEventArgs $eventArgs)
