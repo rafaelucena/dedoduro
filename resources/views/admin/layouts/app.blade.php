aa5b3391f14f51b4d7c5c0ba420d13aad8d342af
rafaelucena <rafael.boszko@gmail.com>
2018-07-28 23:48:26 +0200

    "#0004 - Removing Eloquent entities to set Doctrine entities (Using objects and not arrays)"

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -27,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}" data-toggle="tooltip" title="View Homepage">
-                    {{ app('global_settings')[0]['setting_value'] }} - Admin
+                    {{ app('global_settings')[0]->settingValue }} - Admin
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
