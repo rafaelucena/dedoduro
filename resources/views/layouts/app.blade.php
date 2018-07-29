4210297d709ccba5c6d31e0d3ea94f179f9db6a5
rafaelucena <rafael.boszko@gmail.com>
2018-07-29 22:58:14 +0200

    "#0005 - Adapting Doctrine usage (global_settings nonsense)"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,32 +12,32 @@
     <html itemscope itemtype="http://schema.org/Article">
 
     <!-- SEO Title -->
-    <title>@yield('pageTitle', app('global_settings')[0]['setting_value'].' - '.app('global_settings')[1]['setting_value'])</title>
+    <title>@yield('pageTitle', app('global_settings')[0]->settingValue . ' - ' . app('global_settings')[1]->settingValue)</title>
 
     <!-- SEO Meta Descrition -->
-    <meta name="description" content="@yield('pageDescription', app('global_settings')[1]['setting_value'])">
-
+    <meta name="description" content="@yield('pageDescription', app('global_settings')[1]->settingValue)">
+    
     <!-- Social Meta Tags -->
 
     <!-- Schema.org markup for Google+ -->
-    <meta itemprop="name" content="@yield('pageTitle', app('global_settings')[0]['setting_value'].' - '.app('global_settings')[1]['setting_value'])">
-    <meta itemprop="description" content="@yield('pageDescription', app('global_settings')[1]['setting_value'])">
+    <meta itemprop="name" content="@yield('pageTitle', app('global_settings')[0]->settingValue.' - '.app('global_settings')[1]->settingValue)">
+    <meta itemprop="description" content="@yield('pageDescription', app('global_settings')[1]->settingValue)">
     <meta itemprop="image" content="http://www.example.com/image.jpg">
 
     <!-- Twitter Card data -->
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="@yield('pageTitle', app('global_settings')[0]['setting_value'].' - '.app('global_settings')[1]['setting_value'])">
-    <meta name="twitter:description" content="@yield('pageDescription', app('global_settings')[1]['setting_value'])">
+    <meta name="twitter:title" content="@yield('pageTitle', app('global_settings')[0]->settingValue.' - '.app('global_settings')[1]->settingValue)">
+    <meta name="twitter:description" content="@yield('pageDescription', app('global_settings')[1]->settingValue)">
     <!-- Twitter summary card with large image must be at least 280x150px -->
     <meta name="twitter:image:src" content="http://www.example.com/image.jpg">
 
     <!-- Open Graph data -->
-    <meta property="og:title" content="@yield('pageTitle', app('global_settings')[0]['setting_value'].' - '.app('global_settings')[1]['setting_value'])" />
+    <meta property="og:title" content="@yield('pageTitle', app('global_settings')[0]->settingValue.' - '.app('global_settings')[1]->settingValue)" />
     <meta property="og:type" content="article" />
     <meta property="og:url" content="{{ url()->full() }}" />
     <meta property="og:image" content="@yield('pageImage', 'http://placehold.it/700x700')" />
-    <meta property="og:description" content="@yield('pageDescription', app('global_settings')[1]['setting_value'])" />
-    <meta property="og:site_name" content="{{ app('global_settings')[0]['setting_value'] }}" />
+    <meta property="og:description" content="@yield('pageDescription', app('global_settings')[1]->settingValue)" />
+    <meta property="og:site_name" content="{{ app('global_settings')[0]->settingValue }}" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -55,7 +55,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ app('global_settings')[0]['setting_value'] }} - <small>{{ app('global_settings')[1]['setting_value'] }}</small>
+                    {{ app('global_settings')[0]->settingValue }} - <small>{{ app('global_settings')[1]->settingValue }}</small>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
