d7a2efc7e6a01741ce1ef6e95f09fafeb0dcb0d9
rboszko <r.boszko@b4b.net.pl>
2018-07-30 14:18:06 +0200

    #0005 - Adapting Doctrine usage (AppService improvements)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,22 +22,20 @@ class AppServiceProvider extends ServiceProvider
         /* @var EntityManager $em */
         $em = app('em');
         // Share sidebar Categories with all views
-        if ($categories = $em->getRepository(Category::class)->findAll()) {
-//            $categories = Category::select('name', 'slug', 'id')->get();
-            View::share('sidebar_categories', $categories);
-        }
+//      $categories = Category::select('name', 'slug', 'id')->get();
+        $categories = $em->getRepository(Category::class)->findAll();
+        View::share('sidebar_categories', $categories);
 
         // Share Settings all views
-        if ($settings = $em->getRepository(Setting::class)->findAll()) {
-            // You can keep this in your filters.php file
-            App::singleton('global_settings', function () use ($settings) {
-//                return Setting::select('setting_name', 'setting_value')->get();
-                return $settings;
-            });
-            // If you use this line of code then it'll be available in any view
-            // as $global_settings but you may also use app('global_settings') as well
-            // View::share('global_settings', app('global_settings'));
-        }
+        // You can keep this in your filters.php file
+        $settings = $em->getRepository(Setting::class)->findAll();
+        App::singleton('global_settings', function () use ($settings) {
+//            return Setting::select('setting_name', 'setting_value')->get();
+            return $settings;
+        });
+        // If you use this line of code then it'll be available in any view
+        // as $global_settings but you may also use app('global_settings') as well
+        // View::share('global_settings', app('global_settings'));
     }
 
     /**
