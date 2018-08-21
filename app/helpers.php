bc02cb907f074d5cb54009894432d6395a0f3be8
rafaelucena <rafael.boszko@gmail.com>
2018-08-22 00:00:24 +0200

    #0000 - Adding helpers for avoiding static calls directly

--- app/helpers.php
+++ app/helpers.php
@@ -12,3 +12,28 @@ if (! function_exists('strslug')) {
         return Str::slug($string);
     }
 }
+
+if (! function_exists('storage_put')) {
+    /**
+     * @param string $string
+     * @param $file
+     * @return mixed
+     */
+    function storage_put(string $string, $file) {
+        // ...
+
+        return Storage::putFile($string, $file);
+    }
+}
+
+if (! function_exists('storage_del')) {
+    /**
+     * @param string $string
+     * @return string
+     */
+    function storage_del(string $string) {
+        // ...
+
+        return Storage::delete($string);
+    }
+}
