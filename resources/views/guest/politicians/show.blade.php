e82fbe09ac52059bce94c591f7c3f27aea1d97a8
rafaelucena <rafael.boszko@gmail.com>
2018-09-01 23:14:27 +0200

    "#0016 - Datatables for recent news on Politicians (Experience component)"

--- resources/views/guest/politicians/show.blade.php
+++ resources/views/guest/politicians/show.blade.php
@@ -84,69 +84,7 @@
 
         @include('guest/politicians/show/show-about')
 
-        <div id="experience" class="crt-paper-layers crt-animate">
-            <div class="crt-paper clearfix">
-                <div class="crt-paper-cont paper-padd clear-mrg">
-
-                    <section class="section padd-box">
-                        <h2 class="title-lg text-upper">work experience</h2>
-                        <div class="education">
-
-                            <div class="education-box">
-                                <time class="education-date" datetime="2014-01T2016-03">
-                                    <span>Jan <strong class="text-upper">2014</strong> - Mar <strong>2016</strong></span>
-                                </time>
-                                <h3>Full Stack Developer</h3>
-                                <div class="education-logo">
-                                    <img src="{{ asset('assets/images/uploads/experience/logo-audio-jungle.png') }}" alt="">
-                                </div>
-                                <span class="education-company">IBBA Group</span>
-                                <p>Your brand is the core of your marketing,
-                                    the central theme around your products and services.
-                                    Your brand is not your Logo or your Company Name
-                                </p>
-                            </div>
-                            <!-- .education-box -->
-
-                            <div class="education-box">
-                                <time class="education-date" datetime="2014-01T2016-03">
-                                    <span>Jan <strong class="text-upper">2014</strong> - Mar <strong>2016</strong></span>
-                                </time>
-                                <h3>Systems Analyst / Web Developer</h3>
-                                <div class="education-logo">
-                                    <img src="{{ asset('assets/images/uploads/experience/logo-themeforest.png') }}" alt="">
-                                </div>
-                                <span class="education-company">Loft Studio</span>
-                                <p>
-                                    Your brand is the core of your marketing, the central theme around your products and services.
-                                </p>
-                            </div>
-                            <!-- .education-box -->
-
-                            <div class="education-box">
-                                <time class="education-date" datetime="2014-01T2016-03">
-                                    <span>Jan <strong class="text-upper">2014</strong> - Mar <strong>2016</strong></span>
-                                </time>
-                                <h3>Full Stack Developer</h3>
-                                <div class="education-logo">
-                                    <img src="{{ asset('assets/images/uploads/experience/logo-envato.png') }}" alt="">
-                                </div>
-                                <span class="education-company">Stu Unger Rise</span>
-                                <p>Your brand is the core of your marketing, the central theme around your products and services.</p>
-                            </div>
-                            <!-- .education-box -->
-
-                        </div>
-                        <!-- .education -->
-                    </section>
-                    <!-- .section -->
-
-                </div>
-                <!-- .crt-paper-cont -->
-            </div>
-            <!-- .crt-paper -->
-        </div>
-        <!-- .crt-paper-layers -->
+        @include('guest/politicians/show/show-experience')
 
         <div id="portfolio" class="crt-paper-layers crt-animate">
             <div class="crt-paper clearfix">
