0a884db21c4e5b27ab7471b6d2233f61f86bc251
rafaelucena <rafael.boszko@gmail.com>
2018-09-09 14:06:16 +0200

    #0030 - Working on Search

--- resources/views/guest/home/search.blade.php
+++ resources/views/guest/home/search.blade.php
@@ -8,17 +8,19 @@
 
                     <div class="padd-box-sm">
                         <header class="search-for">
-                            <h1 class="search-title text-muted">search results for:
+                            <h1 class="search-title text-muted">procurando por:
                                 <span>Lorem ipsum</span></h1>
                         </header>
                         <div class="search-result">
-                            <strong class="title-lg text-upper">nothing found</strong>
+                            <strong class="title-lg text-upper">vish... deu ruim...</strong>
+                            <strong class="title-lg text-muted">ENCONTRAMOS UM TOTAL DE:<br><br>0 resultados</strong>
                             <form class="search-again">
                                 <div class="form-item-wrap">
-                                    <input class="form-item" id="searchInput" name="searchInput" type="search" placeholder="Search" value="" size="30" maxlength="80" required="required">
+                                    <input class="form-item" id="searchInput" name="searchInput" type="search"
+                                           placeholder="Busque por nome ou por título da notícia" value="" size="30" maxlength="80" required="required">
                                 </div>
                                 <div class="form-submit form-item-wrap">
-                                    <input class="btn btn-primary" name="submit" type="submit" id="submit" value="Try Again">
+                                    <input class="btn btn-default" name="submit" type="submit" id="submit" value="Agora vai!">
                                 </div>
                             </form>
                         </div>
