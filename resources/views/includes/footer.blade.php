798d118c626d1fa55568f46d148047dd40f112bb
rboszko <r.boszko@b4b.net.pl>
2018-09-12 15:49:13 +0200

    #0043 (fix) - Need to add backend style ASAP

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <hr>
 <p class="text-center">
-    &copy; {{ date('Y') }}. All Rights are reserved to {{ app('global_settings')[0]->settingValue }}. created by <a href="http://mubbiqureshi.com/" target="_blank">Mubbasher Ahmed Qureshi</a>
+    &copy; {{ date('Y') }}. All Rights reserved. Created by Rafael Boszko
 </p>
