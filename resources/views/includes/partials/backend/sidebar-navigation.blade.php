cf7f5f481a478f9f5941d8aa3d35b34cb793bc1f
rafaelucena <rafael.boszko@gmail.com>
2018-09-16 21:49:05 +0200

    #0054 (add) - Moving navigation to partial

---
+++ resources/views/includes/partials/backend/sidebar-navigation.blade.php
@@ -0,0 +1,119 @@
+<aside id="slide-out" class="side-nav white fixed">
+    <div class="side-nav-wrapper">
+        <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+            <li class="nav-title">Principal</li>
+            <li class="no-padding">
+                <a class="collapsible-header waves-effect waves-grey active"><i class="fas fa-user-tie"></i>Políticos</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/form-upload.html">Novo</a></li>
+                        <li><a href="form-basic.html">Lista</a></li>
+                    </ul>
+                </div>
+            </li>
+            {{--<li class="nav-title">Main Navigation</li>--}}
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">dashboard</i>Dashboard</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/index.html">Dashboard v1</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/index-v2.html">Dashboard v2</a></li>--}}
+
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+            {{--<li class="no-padding"><a class="waves-effect waves-grey" href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/widget.html"><i class="material-icons">extension</i>Widgets</a></li>--}}
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">pie_chart</i>Charts</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/chart-flot.html">Flot Chart</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/chart-morris.html">Morris Chart</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/chart-chartjs.html">ChartJs</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/chart-sparkline.html">Sparkline</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/chart-c3.html">c3 Charts</a></li>--}}
+
+
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">grid_on</i>Tables</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/table-basic.html">Basic Tables</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/table-responsive.html">Responsive tables</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/table-data.html">Data-tables</a></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">settings</i>Material UI</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-accordions.html">Accordion</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-badges.html">Badges</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-buttons.html">Buttons</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-typography.html">Typography</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-cards.html">Cards</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-dialogs.html">Dialogs</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-grid.html">Grid</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-helpers.html">Helpers</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-modals.html">Modals</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-media.html">Media</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-icons.html">Icons</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-parallax.html">Parallax</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ui-tabs.html">Tabs</a></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">build</i>Other Elements</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/sweet-alerts.html">Sweet Alerts</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/ion-sliders.html">Sliders</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/nestable.html">Nastable</a></li>--}}
+
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/draggable-cards.html">draggable cards</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/tree-view.html">tree view</a></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+
+            {{--<li class="nav-title">Others</li>--}}
+            {{--<li class="no-padding"><a class="waves-effect waves-grey" href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/calendar.html"><i class="material-icons">perm_contact_calendar</i>Calendar</a></li>--}}
+
+
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">folder</i>Pages</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/page-404.html">404 Page</a></li>--}}
+
+
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/page-login.html">Login</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/page-register.html">Register</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/page-lock-screen.html">Lock Screen</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/page-forgot-password.html">Forgot Password</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/page-pricing.html">Pricing Tables</a></li>--}}
+
+
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+            {{--<li class="no-padding">--}}
+                {{--<a class="collapsible-header waves-effect waves-grey"><i class="material-icons">navigation</i>Maps</a>--}}
+                {{--<div class="collapsible-body">--}}
+                    {{--<ul>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/map-google.html">Google Maps</a></li>--}}
+                        {{--<li><a href="http://bootstraplovers.com/themeforest-html/ultima-v1.2/map-vector.html">Vector Maps</a></li>--}}
+                    {{--</ul>--}}
+                {{--</div>--}}
+            {{--</li>--}}
+            <li class="footer center">
+                <span class="grey-text">&copy; 2016-2017. Ultima</span>
+            </li>
+        </ul>
+    </div>
+</aside>
