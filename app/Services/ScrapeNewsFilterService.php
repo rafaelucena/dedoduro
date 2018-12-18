3f6d5c52a685dc5defbad057cf6f0f8375e6cc00
rafaelucena <rafael.boszko@gmail.com>
2018-12-19 00:36:10 +0100

    #0074 (add) - Changes to allow log of all mapped information (new service class)

---
+++ app/Services/ScrapeNewsFilterService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+
+class ScrapeNewsFilterService
+{
+    private $source;
+
+    private $ignored = [];
+
+    private $updated = [];
+
+    private $created = [];
+
+    public function __construct(array $news)
+    {
+
+    }
+
+    private function filter()
+    {
+
+    }
+}
