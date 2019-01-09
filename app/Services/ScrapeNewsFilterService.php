80b710f2f2428774c35979b4c830a096293e43ce
rafaelucena <rafael.boszko@gmail.com>
2019-01-09 22:57:06 +0100

    #0074 (add) - Changes to allow log of all mapped information (create layout for table of log)

--- app/Services/ScrapeNewsFilterService.php
+++ app/Services/ScrapeNewsFilterService.php
@@ -3,24 +3,40 @@
 namespace App\Services;
 
 
+use App\Http\Models\News;
+use App\Http\Models\Persona;
+use App\Http\Models\PersonaNews;
 use App\Http\Models\Source;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ScrapeNewsFilterService
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
     private $source;
+
+    private $persona;
 
     private $imported = [];
 
-    private $ignored = [];
+    private $ignore = [];
 
-    private $updated = [];
+    private $update = [];
 
-    private $created = [];
+    private $create = [];
 
-    public function __construct(array $resource)
+    public function __construct(EntityManagerInterface $em)
+    {
+    }
+
+    public function filterNews(array $resource)
     {
         $this->setSource($resource['source']);
-        $this->filter($resource['news']);
+        $this->setPersona($resource['persona']);
+        $this->mapNews($resource['news']);
     }
 
     private function setSource(Source $source)
@@ -28,9 +44,48 @@ class ScrapeNewsFilterService
         $this->source = $source;
     }
 
-    private function filter(array $news)
+    private function setPersona(Persona $persona)
+    {
+        $this->persona = $persona;
+    }
+
+    private function mapNews(array $news)
     {
         $this->imported = $news;
+        $hashes = array_column($news, 'hashMd5');
+
+        $found = $this->em->createQueryBuilder()
+            ->select([
+                'ne.hashMd5',
+            ])
+            ->from(News::class, 'ne', 'ne.hashMd5')
+            ->where('ne.hashMd5 IN (:hashes)')
+            ->setParameters([
+                'hashes' => $hashes,
+            ])
+            ->getQuery()
+            ->getResult();
+
+        $result['create'] = array_diff_key($news, $found);
+
+        if (count($result['create']) != count($news)) {
+            $found = $this->em->createQueryBuilder()
+                ->select([
+                    'ne.hashMd5',
+                ])
+                ->from(News::class, 'ne', 'ne.hashMd5')
+                ->innerJoin(PersonaNews::class, 'pn', 'WITH', 'pn.news = ne')
+                ->where('ne.hashMd5 IN (:hashes)')
+                ->andWhere('pn.persona = :persona')
+                ->setParameters([
+                    'hashes' => $hashes,
+                    'persona' => $this->persona,
+                ])
+                ->getQuery()
+                ->getResult();
+
+            $result['relate'] = array_diff_key($news, $found);
+        }
     }
 
     private function setImported(array $news)
